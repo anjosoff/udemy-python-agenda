6c652b633a6d2f52d273f77a7c40c78eb3355b4c
Guilherme dos Anjos <40221817+anjosoff@users.noreply.github.com>
2022-06-25 02:22:19 -0300

    Projeto Agenda

---
+++ contatos/admin.py
@@ -0,0 +1,14 @@
+import imp
+from django.contrib import admin
+from .models import Categoria, Contato
+
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display= ('id','nome','sobrenome','telefone','email',
+                    'datacriacao','categoria','mostrar')
+    list_display_links=('id','nome','sobrenome',)
+    list_editable= ('telefone','mostrar')
+    list_per_page=10
+    search_fields=('id','nome','sobrenome','telefone')
+admin.site.register(Categoria)
+admin.site.register(Contato, ContatoAdmin)
